59f720db98e00d7c26ed1df92c084179d438d1de
DickyDinh <68648708+MrDinhHoa@users.noreply.github.com>
2023-04-13 16:25:16 +0700

    Update

---
+++ 01.Lesson01_ThietKeGiaoDien/MVVM/View/VMain.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ChatGPT.MVVM.View
+{
+    /// <summary>
+    /// Interaction logic for VMain.xaml
+    /// </summary>
+    public partial class VMain : Window
+    {
+        public VMain()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
