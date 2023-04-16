ec652812d9366db470f811c2ad3d56927e56dbd2
MrDinhHoa <dinhhoanghoa@gmail.com>
2023-04-16 14:54:01 +0700

    Update

---
+++ 03.Lesson03_BindingNangCao/MVVM/Model/NUnitTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Security.RightsManagement;
+using System.Text;
+using System.Threading.Tasks;
+using static _03.Lesson03_BindingNangCao.MVVM.Model.TinhTong;
+
+namespace _03.Lesson03_BindingNangCao.MVVM.Model
+{
+    class NUnitTest
+    {
+        public void TinhTong()
+        {
+            int a = 1;
+            int b = 5;
+            int kq = Cong(a, b);
+            Console.WriteLine(kq);
+        }
+    }
+
+    class TinhTong
+    {
+        public static int Cong(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
