1f289034e92094d807dc13e93e5400c7ca68fdbd
MrDinhHoa <dinhhoanghoa@gmail.com>
2023-04-17 22:27:05 +0700

    UP

---
+++ 04.Lesson04_UserManager/MVVM/ViewModel/vmMain.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BIMSoftLib;
+
+namespace _04.Lesson04_UserManager.MVVM.ViewModel
+{
+    class vmMain: PropertyChangeBase
+    {
+        public static vmMain DcMain = new vmMain();
+    }
+}
