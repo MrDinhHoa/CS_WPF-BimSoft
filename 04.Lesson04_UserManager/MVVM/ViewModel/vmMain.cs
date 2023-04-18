0af9af020117b2a203320df71cb6f0273bcd4131
DickyDinh <68648708+MrDinhHoa@users.noreply.github.com>
2023-04-18 17:49:10 +0700

    Update

--- 04.Lesson04_UserManager/MVVM/ViewModel/vmMain.cs
+++ 04.Lesson04_UserManager/MVVM/ViewModel/vmMain.cs
@@ -3,11 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using BIMSoftLib;
+using BIMSoftLib.MVVM;
 
 namespace _04.Lesson04_UserManager.MVVM.ViewModel
 {
-    class vmMain: PropertyChangeBase
+    class vmMain: PropertyChangedBase
     {
         public static vmMain DcMain = new vmMain();
     }
