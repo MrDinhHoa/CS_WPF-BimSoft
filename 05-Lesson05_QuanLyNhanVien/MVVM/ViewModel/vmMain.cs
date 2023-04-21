1530e79552ba4b071ceb6613780122059fc5be12
MrDinhHoa <dinhhoanghoa@gmail.com>
2023-04-21 21:16:25 +0700

    Update

---
+++ 05-Lesson05_QuanLyNhanVien/MVVM/ViewModel/vmMain.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using BIMSoftLib;
+using BIMSoftLib.MVVM;
+
+namespace _05_Lesson05_QuanLyNhanVien.MVVM.ViewModel
+{
+    class vmMain: PropertyChangedBase
+    {
+        public static vmMain DcMain = new vmMain();
+
+    }
+}
