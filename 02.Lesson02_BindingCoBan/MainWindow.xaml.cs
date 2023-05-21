07127d621b9f4ff658a8a6d47f1703c6e6396659
MrDinhHoa <dinhhoanghoa@gmail.com>
2023-05-21 22:05:32 +0700

    Update

--- 02.Lesson02_BindingCoBan/MainWindow.xaml.cs
+++ 02.Lesson02_BindingCoBan/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace _02.Lesson02_BindingCoBan
+namespace Lesson02_BindingCoBan
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
--- Lesson09_AcadNET/Lesson09_AcadNET.cs
+++ Lesson09_AcadNET/Lesson09_AcadNET.cs
@@ -1,18 +1,37 @@
-﻿using Autodesk.AutoCAD.Runtime;
+﻿using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.Runtime;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Forms;
+using Application = Autodesk.AutoCAD.ApplicationServices.Application;
 
 namespace Lesson09_AcadNET
 {
     public class Lesson09_AcadNET
     {
         [CommandMethod("TKThepDam")]
+
         public void TKThepDam()
         {
+            MessageBox.Show("Đã bật được CAD","Lỗi",MessageBoxButtons.OK,MessageBoxIcon.Error);
+            var adoc = Application.DocumentManager.MdiActiveDocument;
+            Editor editor = adoc.Editor;
 
+            //Create the Prompt;
+            PromptKeywordOptions pko = new PromptKeywordOptions("Chọn command");
+            pko.Keywords.Add("Command1");
+            pko.Keywords.Add("comMand2");
+            pko.AllowNone = false;
+
+            //Get the user input
+            PromptResult result = editor.GetKeywords(pko);
+            if(result.Status == PromptStatus.OK)
+            {editor.WriteMessage($"Nhập({ result.StringResult})"); }
+            else { editor.WriteMessage("Không nhập gì"); }
         }
 
 
